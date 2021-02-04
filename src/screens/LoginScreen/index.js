b0ac4e20f09b5a3c5cc7c6617ddc23269a973924
Matheus Marrane <matheus.chagas@topaziopartner.com.br>
2021-02-04 15:40:47 -0300

    Install react-navigation and create screen files

---
+++ src/screens/LoginScreen/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export const LoginScreen = (props) => {
+  return <></>;
+};
---
+++ src/screens/TransactionDetailScreen/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export const TransactionDetailScreen = (props) => {
+  return <></>;
+};
